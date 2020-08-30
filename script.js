378114d6b0d482af8e0e3a15b4f47a9512c6a0ea
Andriy <klym.andriy14@gmail.com>
2020-08-30 23:31:49 +0300

    the second part of the task

--- script.js
+++ script.js
@@ -17,9 +17,13 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
      const a = prompt('Один из последних просмотренных фильмов?', ''),
            b = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
  
-
+     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+         console.log('done');
+     } else {
+         console.log('error');
+         i--;
+     }
+ }
  
