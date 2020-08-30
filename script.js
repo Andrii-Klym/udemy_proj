eeb400c3c2e7ec23aaac42845398a2cc1a2c500e
Andriy <klym.andriy14@gmail.com>
2020-08-30 23:28:23 +0300

    the first part of the task

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const personalMovieDB = {
      private: false
 };
 
-const a = prompt('Один із переглянутих фільмів', ''),
-      b = +prompt('на скільки ви його оціните', ''),
+/* const a = prompt('Один із переглянутих фільмів', ''),
+      b = prompt('на скільки ви його оціните', ''),
       c = prompt('Один із переглянутих фільмів', ''),
-      d = +prompt('на скільки ви його оціните', '');
+      d = prompt('на скільки ви його оціните', '');
+ */
+
+for (let i = 0; i < 2; i++) {
+     const a = prompt('Один из последних просмотренных фильмов?', ''),
+           b = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+ 
 
-console.log(personalMovieDB)
+ 
